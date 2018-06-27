5b5ad112e16944592b8cd8643b289ce76b8f8e39
James <james.iter.cn@gmail.com>
2018-06-27 15:53:03 +0800

    @

--- hyperledger_fabric/v1.1.0/auto_install.sh
+++ hyperledger_fabric/v1.1.0/auto_install.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 make setup
+make gen_config
+make start
 
+make test_channel_create 
+make test_channel_join
+make test_cc_install
+make test_cc_instantiate
+make test_cc_invoke_query
+
+make test_lscc
+make test_qscc
+
+make test_fetch_blocks
+
+yum install jq -y
+make test_configtxlator
+
+# make stop
+# make clean
